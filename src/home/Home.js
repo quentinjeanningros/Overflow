aff5f05c89555f7b0ce19110f081801e7980232c
quentinJeanningros <quentin.jeanningros@epitech.eu>
2019-12-25 01:36:12 +0100

    all navigation are done

--- src/home/Home.js
+++ src/home/Home.js
@@ -11,7 +11,53 @@ class Home extends React.Component {
     render() {
         return (
             <div className="background black-color--back">
+                <div className="home-button-container">
+                    <NavButtonHome text="Events" path="/events"/>
+                    <NavButtonHome text="Discounts" path="/partnership"/>
+                </div>
+            </div>
+        )
+    }
+}
 
+class NavButtonHome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false
+        }
+        this.text = props.text
+        this.path = props.path
+
+        this.toggleHover = this.toggleHover.bind(this);
+        this.clicked = this.clicked.bind(this);
+    }
+
+    toggleHover() {
+        this.setState({hover: !this.state.hover})
+    }
+
+    clicked() {
+        window.location.href = this.path;
+    }
+
+    render() {
+        let classContainer = "nav-button-home__marging"
+        if (this.state.hover)
+            classContainer += "--hover "
+        return (
+            <div className="nav-button-home-container"
+                onMouseEnter={this.toggleHover}
+                onMouseLeave={this.toggleHover}
+                onClick={this.clicked}>
+                <div className="nav-button-home__marging">
+                    <div className="blue-color--back nav-button-home--square"/>
+                    <h3 className="blue-color select-none nav-button-home--text">{this.text}</h3>
+                </div>
+                <div className={classContainer}>
+                    <div className="white-color--back nav-button-home--square"/>
+                    <h3 className="white-color select-none nav-button-home--text">{this.text}</h3>
+                </div>
             </div>
         )
     }
