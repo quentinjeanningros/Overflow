d0a2e24177dd78a87e4174237e343220e8429585
quentinJeanningros <quentin.jeanningros@epitech.eu>
2019-12-29 20:49:40 +0100

    partner carousel

--- src/partnership/PartnerCarrousel.js
+++ src/partnership/PartnerCarrousel.js
@@ -20,17 +20,39 @@ class PartnerCarrousel extends React.Component {
     }
 
     render() {
-        let i = 0;
         let listoDisplay = []
-        this.partners.forEach(element => {
-            listoDisplay.push(<PartnerCard
-                name={element.name}
-                key={i}/>);
-            ++i
-        });
+        for (let i = 0; i < 5 && i < this.partners.length; ++i) {
+            if (i === 2) {
+                listoDisplay.push(<PartnerCardFocused name={this.partners[i].name} key={i}/>);
+            } else {
+                listoDisplay.push(<PartnerCard name={this.partners[i].name} key={i}/>);
+            }
+        }
         return (
-            <div className="partner-carrousel__container">
+            <div className="partner-carrousel--container">
                 {listoDisplay}
+            </div>
+        )
+    }
+}
+
+class PartnerCardFocused extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+        this.name = props.name
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="partner-card-focused--text--container">
+                    <div className="partner-card-focused--square blue-color--back"/>
+                    <h1 className="partner-card-focused--text black-color font-first select-none">{this.name}</h1>
+                </div>
+                <div className="partner-card-focused--background white-color--back">
+                </div>
             </div>
         )
     }
@@ -46,7 +68,9 @@ class PartnerCard extends React.Component {
 
     render() {
         return (
-            <div className="partner-card__background">
+            <div className="partner-card--background black-color--back">
+                <div className="partner-card--square white-color--back"/>
+                <h1 className="partner-card--text white-color font-first select-none">{this.name}</h1>
             </div>
         )
     }
--- src/partnership/Partnership.js
+++ src/partnership/Partnership.js
@@ -11,19 +11,22 @@ class Partnership extends React.Component {
         this.linkedPages = [new Link("Home", "/home"),
             new Link("Events", "events")]
         this.partners = [new Partner("Barberousse", true, true, true, ""),
-            new Partner("Flam's", true, true, true, ""),
-            new Partner("McDonald's", true, true, true, ""),
+            new Partner("Flam's", true, false, false, ""),
+            new Partner("McDonald's", true, false, true, ""),
             new Partner("Chez Victor", true, true, true, ""),
+            new Partner("Nooï", true, false, false, ""),
             new Partner("Le Phonographe", true, true, true, "")]
     }
 
     render() {
         return (
-            <div className="background white-color--back">
+            <div id="partnership-page" className="background white-color--back">
                 <div className="square-title black-color--back"/>
                 <h1 className="page-title font-second black-color select-none">Partnership</h1>
                 <NavigationBar color="black-color" triggerColor="blue-color" links={this.linkedPages}/>
-                <PartnerCarrousel partners={this.partners}/>
+                <div className="partnership--carrousel">
+                    <PartnerCarrousel partners={this.partners}/>
+                </div>
             </div>
         )
     }
