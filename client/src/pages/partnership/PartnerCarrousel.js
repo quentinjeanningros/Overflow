8d9e8b4e7c677f2a27f7a604c2c1cdcb150d9c00
quentinjeanningros <quentin.jeanningros@epitech.eu>
2020-01-12 18:19:52 +0100

    solved bug flip on macOS Opera

--- client/src/pages/partnership/PartnerCarrousel.js
+++ client/src/pages/partnership/PartnerCarrousel.js
@@ -257,7 +257,7 @@ class PartnerCardFocused extends React.Component {
                         </div>
                     </div>
                     <div className="back">
-                        <p className="partner-card-focused--description white-color font-first">{this.state.partner.description}</p>
+                        <p className="partner-card-focused--description white-color font-first back-hidden">{this.state.partner.description}</p>
                     </div>
                 </div>
                 </div>
